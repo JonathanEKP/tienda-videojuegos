a19111b162fa562307473f9fc5050587c9f0d70a
LAPTOP-S7ES90NR\edgar <adogonza1124@gmail.com>
2021-05-17 16:35:56 -0600

    Añadiendo frmAdminPrincipal

---
+++ VideoGamesStore/FormulariosAdmin/frmAdminPrincipal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoGamesStore.FormulariosAdmin
+{
+    public partial class frmAdminPrincipal : Form
+    {
+        public frmAdminPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAdminPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
